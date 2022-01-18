6676a47206e54cbb934ec9259a1b6631380321cf
Luisa Alarcon <luisaalarcons30@gmail.com>
2022-01-18 15:05:22 -0500

    Put the script for calculating most demanded floors into a function. Other minor changes on variable names.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime
 from random import randint
 import pandas as pd
 from collections import defaultdict, Counter
+import time
 
 
 class Elevator:
@@ -24,12 +25,33 @@ def generate_db():
     timestamps = [datetime.datetime.fromtimestamp(x) for x in epochs]
     floor_requested = [randint(1, 10) for x in range(1000)]
     data = {'Timestamp': timestamps, 'Floor_requested': floor_requested}
-    db = pd.DataFrame(data)
-    return db
+    database = pd.DataFrame(data)
+    return database
+
+
+def most_demanded_floors(database):
+    hours = [database.Timestamp[x].hour for x in range(len(database.Timestamp))]
+    floors = [database.Floor_requested[x] for x in range(len(database.Floor_requested))]
+    floor_counts_per_hour = defaultdict(Counter)
+    for hour, floor in zip(hours, floors):
+        floor_counts_per_hour[hour][floor] += 1
+    mcf_per_hour_with_counts = {}  # Most common floor per hour dictionary, w/ occurrence counts per floor
+    for hour, counts in floor_counts_per_hour.items():
+        most_common = counts.most_common(1)
+        mcf_per_hour_with_counts[hour] = most_common
+    '''final = {}
+    for k, v in mcf_per_hour_with_counts.items():
+        values = v[0][0]
+        final[k] = values
+    print(values)'''
+    extracted_mcfloors = [i[0][0] for i in mcf_per_hour_with_counts.values()]
+    mcf_per_hour = dict(zip(mcf_per_hour_with_counts.keys(), extracted_mcfloors))
+    return mcf_per_hour
+    # mcf_per_hour is the final relational db of the most popular floor per each registered hour, only ints
 
 
 def move_to_passenger_floor(passenger_floor):
-    pass
+    print('You are currently in floor ' + passenger)
 
 
 def move_to_db_floor(db_floor):
@@ -38,26 +60,5 @@ def move_to_db_floor(db_floor):
 
 if __name__ == '__main__':
     db = generate_db()
-    # print(db)
-    # pd.set_option('display.max_rows', None)
-    # Next is going to go in a function other than main, here for now just for testing purposes
-    hours = [db.Timestamp[x].hour for x in range(len(db.Timestamp))]
-    floors = [db.Floor_requested[x] for x in range(len(db.Floor_requested))]
-    floor_counts_per_hour = defaultdict(Counter)
-    for hour, floor in zip(hours, floors):
-        floor_counts_per_hour[hour][floor] += 1
-    print(floor_counts_per_hour)
-    mcf_per_hour_with_counts = {}  # Most common floor per hour dictionary, w/ occurrence counts per floor
-    for hour, counts in floor_counts_per_hour.items():
-        most_common = counts.most_common(1)
-        mcf_per_hour_with_counts[hour] = most_common
-    print(mcf_per_hour_with_counts)
-    '''final = {}
-    for k, v in mcf_per_hour_with_counts.items():
-        values = v[0][0]
-        final[k] = values
-    print(values)'''
-    extracted_mcfloors = [i[0][0] for i in mcf_per_hour_with_counts.values()]
-    mcf_per_hour = dict(zip(mcf_per_hour_with_counts.keys(), extracted_mcfloors))
-    # mcf_per_hour is the final relational db of the most popular floor per each registered hour, only ints
-    print(mcf_per_hour)
+    # pd.set_option('display.max_rows', None) -- to see full dataframe after print is used
+    mcf_x_hour = most_demanded_floors(db)
